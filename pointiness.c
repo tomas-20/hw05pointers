de92dd4e12a64830562dc69125a3d4a636b4732e
tomas-20 <tacuna20@stuy.edu>
2021-10-05 17:16:48 -0400

    printed the addresses for the char, int, and long

--- pointiness.c
+++ pointiness.c
@@ -3,5 +3,8 @@ int main() {
   char charzard = 'e';
   int inti = 42;
   long long_er = 2187;
+  printf("our char's address is %p which is %lu in decimal\n", &charzard, &charzard);
+  printf("our intt's address is %p which is %lu in decimal\n", &inti, &inti);
+  printf("our long's address is %p which is %lu in decimal\n", &long_er, &long_er);
   return 0;
 }
