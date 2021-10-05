c056db4f84c651fadc56d52ed6a65f5a70f71de0
tomas-20 <tacuna20@stuy.edu>
2021-10-05 17:21:18 -0400

    fixed the grevious error of misspelling 'charizard'

--- pointiness.c
+++ pointiness.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
-  char charzard = 'e';
+  char charizard = 'e';
   int inti = 42;
   long long_er = 2187;
-  printf("our char's address is %p which is %lu in decimal\n", &charzard, &charzard);
+  printf("our char's address is %p which is %lu in decimal\n", &charizard, &charizard);
   printf("our intt's address is %p which is %lu in decimal\n", &inti, &inti);
   printf("our long's address is %p which is %lu in decimal\n", &long_er, &long_er);
   return 0;
